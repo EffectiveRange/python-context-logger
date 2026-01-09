acfa5c6bb3f7c07a366dc51ab1959ced9c870f62
AttilaGombosER <attila.gombos@effective-range.com>
2026-01-09 16:00:38 +0100

    Use SCM versioning (#17)

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='python-context-logger',
-    version='1.2.1',
     description='Contextual structured logging library for Python',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
     packages=['context_logger'],
     package_data={'context_logger': ['py.typed']},
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
     install_requires=['structlog']
 )
